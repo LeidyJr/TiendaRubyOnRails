c81c474eedf56425f90b8048f72105b95ff1241b
LeidyJr <rivera.leidy@correounivalle.edu.co>
2019-11-23 19:23:25 +0000

    Proyecto base

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :admins
+  namespace :admin do
+    resources :products
+  end
+  resources :products do
+  get 'search', on: :collection
+  end
+  root 'products#index'
+  get 'products/:id/purchase', to: 'products#purchase', as: :purchase_product
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
